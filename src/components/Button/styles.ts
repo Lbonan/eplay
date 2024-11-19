584dffb604a5094fa72bf7d86508c5f41c67c16f
Lucas Bonan <lucasbonan15@gmail.com>
2024-11-19 18:13:54 -0300

    Criando o Hero

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,14 +2,19 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { cores } from '../../styles'
 
-export const ButtonContainer = styled.button`
-  border: 2px solid ${cores.branca};
+import { Props } from '.'
+
+export const ButtonContainer = styled.button<Props>`
+  border: 2px solid
+    ${(props) => (props.variant === 'primary' ? cores.verde : cores.branca)};
   color: ${cores.branca};
-  background-color: transparent;
+  background-color: ${(props) =>
+    props.variant === 'primary' ? cores.verde : 'trasparent'};
   font-size: 16px;
   font-weight: bold;
   padding: 8px 16px;
   border-radius: 8px;
+  cursor: pointer;
 `
 export const ButtonLink = styled(Link)`
   border: 2px solid ${cores.branca};
